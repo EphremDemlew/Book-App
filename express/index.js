ea46a0ac74f4225d79c1fa8028ec78e885b21a3e
Ephrem Demlew <ephyaabb@gmail.com>
2022-08-17 12:33:06 +0300

    fixed cors issue

--- express/index.js
+++ express/index.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
-app.use(cors);
+app.use(cors());
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
